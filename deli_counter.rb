0031fca67e19f0ac70a63c2e06e712a4b47ea508
brookton <davidbrookton@gmail.com>
2019-04-16 10:33:45 -0600

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "there is nobody in line"
+  end
+  if katz_deli.size >= 1
+    line_order = []
+    katz_deli.each_with_index do |index, katz_deli|
+    line_order << "there are people in line #{index}. #{katz_deli}"
+  end
+end
+
+def take_a_number
+end
+
+def now_serving
+end
